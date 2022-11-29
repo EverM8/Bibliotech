76f02ad11545c66931ce0560a6c61b20d4c7303a
EverM8 <evcarvalho93@gmail.com>
2022-11-29 19:02:15 -0300

    Bibliotech

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+   firebaseConfig: {
+    apiKey: "AIzaSyDxc3ViSrMkYAD9s_7OWo5tka_PdALW-e4",
+    authDomain: "bibliotech-77bc8.firebaseapp.com",
+    projectId: "bibliotech-77bc8",
+    storageBucket: "bibliotech-77bc8.appspot.com",
+    messagingSenderId: "887739168883",
+    appId: "1:887739168883:web:b57b649ba83227847dd206"
+  }
+};
